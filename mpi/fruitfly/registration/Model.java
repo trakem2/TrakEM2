de83e8196ad813f2408b11c9c4bf427bfdb4d7e7
U-NONAME\Administrator <Administrator@noname.(none)>
2007-08-20 08:38:39 +0200

    Updated mpi.fruitfly.registration.Model.java and mpi.fruitfly.registration.TRModel.java to provide access to the set of inliers. TODO Model estimation waits for a nice interface implementation...

--- mpi/fruitfly/registration/Model.java
+++ mpi/fruitfly/registration/Model.java
@@ -4,84 +4,84 @@ import java.util.Vector;
 import java.util.Iterator;
 
 abstract public class Model {
-	
-	public float error;        //!< maximal error of this model
 
-    /**
-     * instantiates an empty model with maximally large error
-     */
-    public Model()
-    {
-    	error = Float.MAX_VALUE;
-    }
+	public float error;		//!< maximal error of this model
+	public Vector < Match > inliers;
 
-    static final private int MIN_SET_SIZE = 0;
+	/**
+	 * instantiates an empty model with maximally large error
+	 */
+	public Model()
+	{
+		error = Float.MAX_VALUE;
+	}
 
-    /**
-     * fit the model to a minimal set of FeatureMatches
-     */
-    abstract boolean fit( Vector< Match > matches );
+	static final public int MIN_SET_SIZE = 0;
 
-    /**
-     * apply the model to a location
-     */
-    abstract float[] apply( float[] point );
+	/**
+	 * fit the model to a minimal set of FeatureMatches
+	 */
+	abstract boolean fit( Vector< Match > matches );
 
-    /**
-     * test the model for a set of Matches
-     */
-    public float test( Vector< Match > matches, float epsilon, float min_inliers, Vector< Match > inliers )
-    {
-        int mi = ( int )( min_inliers * matches.size() );
-        
-        int ni = 0;                                                                                                                                                             
-        for ( Iterator< Match > i = matches.iterator(); i.hasNext(); )                                                                                       
-        {
-        	Match m = i.next();
-            
-        	float[] p1t = apply( m.p1 );                                                                                                                                                       
-        	
-        	float te = 0;
-        	for ( int j = 0; j < m.p2.length; ++ j )
-        	{
-        		float d = p1t[ j ] - m.p2[ j ];
-        		te += d * d;
-        	}
-            
-        	//System.out.println( sqrt( te ) );
-            te = ( float )Math.sqrt( te );
-            if ( te < epsilon )
-            {
-            	inliers.addElement( m );
-                ++ni;
-            }
-            if ( ni > mi )
-            {
-            	//error = e / static_cast< float >( ni );
-                error = 1.0f - ( ( float )( ni ) / ( float )( matches.size() ) );
-                if (error > 1.0f)
-                	error = 1.0f;
-                if (error < 0f)
-                	error = 0.0f;
-                
-            }
-        }
-        
-        return error;
-    }
+	/**
+	 * apply the model to a location
+	 */
+	abstract float[] apply( float[] point );
 
-    /**
-     * less than operater to make the models sortable, returns false for error < 0
-     */
-    public boolean betterThan( Model m )
-    {
-    	if ( error < 0 ) return false;
-    	return error < m.error;
-    }
+	/**
+	 * test the model for a set of Matches
+	 */
+	public float test( Vector< Match > matches, float epsilon, float min_inliers, Vector< Match > inliers )
+	{
+		int mi = ( int )( min_inliers * matches.size() );
 
-    /**
-     * string to output stream
-     */
-    abstract public String toString();
+		int ni = 0;
+		for ( Iterator< Match > i = matches.iterator(); i.hasNext(); )
+		{
+			Match m = i.next();
 
-};                                                                                  
+			float[] p1t = apply( m.p1 );
+
+			float te = 0;
+			for ( int j = 0; j < m.p2.length; ++ j )
+			{
+				float d = p1t[ j ] - m.p2[ j ];
+				te += d * d;
+			}
+
+			//System.out.println( "te: " + te );
+			te = ( float )Math.sqrt( te );
+			if ( te < epsilon )
+			{
+				inliers.addElement( m );
+				++ni;
+			}
+			if ( ni > mi )
+			{
+				//error = e / static_cast< float >( ni );
+				error = 1.0f - ( ( float )( ni ) / ( float )( matches.size() ) );
+				if (error > 1.0f)
+					error = 1.0f;
+				if (error < 0f)
+					error = 0.0f;
+			}
+		}
+
+		return error;
+	}
+
+	/**
+	 * less than operater to make the models sortable, returns false for error < 0
+	 */
+	public boolean betterThan( Model m )
+	{
+		if ( error < 0 ) return false;
+		return error < m.error;
+	}
+
+	/**
+	 * string to output stream
+	 */
+	abstract public String toString();
+
+};
