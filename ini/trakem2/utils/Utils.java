4d03ec64eb23ee807f88671e8ee8254ba9e13046
Albert Cardona <acardona@ini.phys.ethz.ch>
2009-05-02 15:59:19 +0200

    Version 0.7d

--- ini/trakem2/utils/Utils.java
+++ ini/trakem2/utils/Utils.java
@@ -80,7 +80,7 @@ import java.util.regex.Matcher;
  */
 public class Utils implements ij.plugin.PlugIn {
 
-	static public String version = "0.7c 2009-04-23";
+	static public String version = "0.7d 2009-05-02";
 
 	static public boolean debug = false;
 	static public boolean debug_mouse = false;
