d02f196e94b53f7b8e52474d4655729f8c30a2bb
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2009-06-06 22:59:59 +0200

    PrecomputedTransformMesh without scaling

--- mpicbg/trakem2/transform/PrecomputedTransformMesh.java
+++ mpicbg/trakem2/transform/PrecomputedTransformMesh.java
@@ -1,10 +1,24 @@
+/**
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package mpicbg.trakem2.transform;
 
 import ij.ImagePlus;
 import ij.process.FloatProcessor;
 import mpicbg.models.CoordinateTransform;
-import mpicbg.models.CoordinateTransformList;
-import mpicbg.models.SimilarityModel2D;
 
 /**
  * Speeds up direct
@@ -17,37 +31,23 @@ import mpicbg.models.SimilarityModel2D;
 public class PrecomputedTransformMesh extends TransformMesh
 {
 	final protected FloatProcessor tx, ty, itx, ity;
-	final protected float scale;
 	
 	public PrecomputedTransformMesh(
 			final CoordinateTransform t,
 			final int numX,
 			final float width,
-			final float height,
-			final float scale )
+			final float height )
 	{
 		super( t, numX, width, height );
 		
-		this.scale = scale;
-		
-		final int scaledWidth = ( int )Math.ceil( scale * width );
-		final int scaledHeight = ( int )Math.ceil( scale * height );
-		
-		final FloatProcessor o = new FloatProcessor( scaledWidth, scaledHeight );
+		final FloatProcessor o = new FloatProcessor( ( int )Math.ceil( width ), ( int )Math.ceil( height ) );
 		final float[] oPixels = ( float[] )o.getPixels();
-		final FloatProcessor m = new FloatProcessor( scaledWidth, scaledHeight );
+		final FloatProcessor m = new FloatProcessor( ( int )Math.ceil( width ), ( int )Math.ceil( height ) );
 		final float[] mPixels = ( float[] )m.getPixels();
-		for ( int i = 0; i < mPixels.length; ++i )
+		for ( int i = 0; i < oPixels.length; ++i )
 			mPixels[ i ] = 1;
 		
-		final SimilarityModel2D sm = new SimilarityModel2D();
-		sm.set( scale, 0, 0, 0 );
-		final CoordinateTransformList ctl = new CoordinateTransformList();
-		ctl.add( sm.createInverse() );
-		ctl.add( t );
-		ctl.add( sm );
-		final TransformMesh tmScaled = new TransformMesh( ctl, numX, scaledWidth, scaledHeight );
-		final TransformMeshMapping mapping = new TransformMeshMapping( tmScaled );
+		final TransformMeshMapping mapping = new TransformMeshMapping( this );
 		
 		final FloatProcessor tm = ( FloatProcessor )mapping.createMappedImageInterpolated( m );
 		final float[] tmPixels = ( float[] )tm.getPixels();
@@ -55,7 +55,7 @@ public class PrecomputedTransformMesh extends TransformMesh
 		
 		/* prepare itx */
 		for ( int i = 0; i < oPixels.length; ++i )
-			oPixels[ i ] = ( int )( i % o.getWidth() ) / scale;
+			oPixels[ i ] = i % o.getWidth();
 		itx = ( FloatProcessor )mapping.createMappedImageInterpolated( o );
 		final float[] itxPixels = ( float[] )itx.getPixels();
 		for ( int i = 0; i < itxPixels.length; ++i )
@@ -64,36 +64,27 @@ public class PrecomputedTransformMesh extends TransformMesh
 		
 		/* prepare ity */
 		for ( int i = 0; i < oPixels.length; ++i )
-			oPixels[ i ] = ( int )( i / o.getWidth() ) / scale;
+			oPixels[ i ] = i / o.getWidth();
 		ity = ( FloatProcessor )mapping.createMappedImageInterpolated( o );
 		final float[] ityPixels = ( float[] )ity.getPixels();
 		for ( int i = 0; i < ityPixels.length; ++i )
 			if ( tmPixels[ i ] == 0 ) ityPixels[ i ] = Float.NaN;
 		new ImagePlus( "ity", ity ).show();
 		
-		final FloatProcessor io = new FloatProcessor( tmScaled.boundingBox.width, tmScaled.boundingBox.height );
+		final FloatProcessor io = new FloatProcessor( boundingBox.width, boundingBox.height );
 		final float[] ioPixels = ( float[] )io.getPixels();
 		
 		/* prepare tx */
 		for ( int i = 0; i < ioPixels.length; ++i )
-			ioPixels[ i ] = ( int )( i % io.getWidth() + tmScaled.boundingBox.x ) / scale;
+			ioPixels[ i ] = i % io.getWidth() + boundingBox.x;
 		tx = ( FloatProcessor )mapping.createInverseMappedImageInterpolated( io );
 		new ImagePlus( "tx", tx ).show();
 		
 		/* prepare ty */
 		for ( int i = 0; i < ioPixels.length; ++i )
-			ioPixels[ i ] = ( int )( i / io.getWidth() + tmScaled.boundingBox.y ) / scale;
+			ioPixels[ i ] = i / io.getWidth() + boundingBox.y;
 		ty = ( FloatProcessor )mapping.createInverseMappedImageInterpolated( io );
 		new ImagePlus( "ty", ty ).show();
-	}
-	
-	public PrecomputedTransformMesh(
-			final CoordinateTransform t,
-			final int numX,
-			final float width,
-			final float height )
-	{
-		this( t, numX, width, height, 2 * numX / width );
 	}
 	
 	@Override
@@ -101,13 +92,10 @@ public class PrecomputedTransformMesh extends TransformMesh
 	{
 		assert location.length == 2 : "2d transform meshs can be applied to 2d points only.";
 		
-		final float xt = location[ 0 ] * scale;
-		final float yt = location[ 1 ] * scale;
-		
-		if ( xt >= 0 && xt < tx.getWidth() && yt >= 0 && yt < tx.getHeight() )
+		if ( location[ 0 ] >= 0 && location[ 0 ] < tx.getWidth() && location[ 1 ] >= 0 && location[ 1 ] < tx.getHeight() )
 		{
-			final float x = ( float )tx.getInterpolatedPixel( xt, yt );
-			final float y = ( float )ty.getInterpolatedPixel( xt, yt );
+			final float x = ( float )tx.getInterpolatedPixel( location[ 0 ], location[ 1 ] );
+			final float y = ( float )ty.getInterpolatedPixel( location[ 0 ], location[ 1 ] );
 			location[ 0 ] = x - boundingBox.x;
 			location[ 1 ] = y - boundingBox.y;
 		}
@@ -120,13 +108,10 @@ public class PrecomputedTransformMesh extends TransformMesh
 	{
 		assert location.length == 2 : "2d transform meshs can be applied to 2d points only.";
 		
-		final float xt = location[ 0 ] * scale;
-		final float yt = location[ 1 ] * scale;
-		
-		if ( xt >= 0 && xt < itx.getWidth() && yt >= 0 && yt < itx.getHeight() )
+		if ( location[ 0 ] >= 0 && location[ 0 ] < itx.getWidth() && location[ 1 ] >= 0 && location[ 1 ] < itx.getHeight() )
 		{
-			final float x = ( float )itx.getInterpolatedPixel( xt, yt );
-			final float y = ( float )ity.getInterpolatedPixel( xt, yt );
+			final float x = ( float )itx.getInterpolatedPixel( location[ 0 ], location[ 1 ] );
+			final float y = ( float )ity.getInterpolatedPixel( location[ 0 ], location[ 1 ] );
 			location[ 0 ] = x;
 			location[ 1 ] = y;
 		}
