29db5c7846a3710fee4b9826930f78ab11df2682
Albert Cardona <acardona@ini.phys.ethz.ch>
2009-09-13 12:27:08 +0200

    Added convenient method to print generic Object for Utils

--- ini/trakem2/utils/Utils.java
+++ ini/trakem2/utils/Utils.java
@@ -278,6 +278,14 @@ public class Utils implements ij.plugin.PlugIn {
 		Utils.log2(sb.toString());
 	}
 
+	static public final void log(final Object ob) {
+		Utils.log(Utils.toString(ob));
+	}
+
+	static public final void log2(final Object... ob){
+		Utils.log2(Utils.toString(ob));
+	}
+
 	/** Print an object; if it's an array, print each element, recursively, as [0, 1, 2] or [[0, 1, 2], [3, 4, 5]], etc, same for Iterable and Map objects. */
 	static public final String toString(final Object ob) {
 		if (null == ob) return "null";
