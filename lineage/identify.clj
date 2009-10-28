de6a9d8ced5725e418fcc44e1bc927afe693908f
Albert Cardona <albert@mac.(none)>
2009-10-28 02:48:24 -0400

    Proper GUI resizing, properly sorted matches.

--- lineage/identify.clj
+++ lineage/identify.clj
@@ -6,8 +6,9 @@
      (javax.swing JTable JFrame JScrollPane JPanel JLabel BoxLayout)
      (javax.swing.table AbstractTableModel)
      (java.awt.event MouseAdapter)
-     (java.awt Color)
+     (java.awt Color Dimension Component)
      (mpicbg.models AffineModel3D)
+     (ini.trakem2.utils Utils)
      (ini.trakem2.display Display Display3D LayerSet)
      (ini.trakem2.vector Compare VectorString3D)))
 
@@ -101,46 +102,66 @@
                                      (vals (into (sorted-map) (select-keys target-fids common-fid-keys)))
                                      AffineModel3D)))))
 
+(defn resample
+  "Returns a resampled copy of vs."
+  [vs delta]
+  (let [copy (.clone vs)]
+    (.resample copy delta)
+    copy))
+
 (defn match-all
-  "Returns a table of SAT-name vs matches of the query-vs against all SAT vs in the library
-  sorted by mean euclidean distance, and labeled as correct of incorrect matches
-  according to the Random Forest classifier."
+  "Returns a vector of two elements; the first element is the list of matches
+  of the query-vs against all SAT vs in the library sorted by mean euclidean distance,
+  and labeled as correct of incorrect matches according to the Random Forest classifier.
+  The second element is a list of corresponding SAT names for each match."
   [query-vs fids delta direct substring]
-  (let [vs1 (register-vs query-vs fids FRT42-fids)]
-    (.resample vs1 delta)
-    (reduce
-      (fn [m match]
-        (assoc m (match :SAT-name) match))
-      {}
-      (sort
-        (proxy [java.util.Comparator] []
-          (equals [o]
-            (= o this))
-          (compare [o1 o2]
-            (int (- (o1 :med) (o2 :med)))))
-        (map
-          (fn [e]
-            (let [vs2 (let [copy (.clone (val e))]
-                        (.resample copy delta)
-                        copy)
-                  c (Compare/findBestMatch vs1 vs2
-                                           (double delta) false (int 5) (float 0.5) Compare/AVG_PHYS_DIST
-                                           direct substring
-                                           (double 1.1) (double 1.1) (double 1))
-                  stats (.getStatistics (get c 0) false (int 0) (float 0) false)]
-              {:SAT-name (key e)
-               :stats stats
-               :med (get stats 0)
-               :correct (LineageClassifier/classify stats)}))
-          SAT-lib)))))
+  (let [vs1 (resample (register-vs query-vs fids FRT42-fids) delta)
+        matches (sort
+                  (proxy [java.util.Comparator] []
+                    (equals [o]
+                      (= o this))
+                    (compare [o1 o2]
+                      (int (- (o1 :med) (o2 :med)))))
+                  (map
+                    (fn [e]
+                      (let [vs2 (let [copy (.clone (val e))]
+                                  (.resample copy delta)
+                                  copy)
+                            c (Compare/findBestMatch vs1 vs2
+                                                     (double delta) false (int 5) (float 0.5) Compare/AVG_PHYS_DIST
+                                                     direct substring
+                                                     (double 1.1) (double 1.1) (double 1))
+                            stats (.getStatistics (get c 0) false (int 0) (float 0) false)]
+                        {:SAT-name (key e)
+                         :stats stats
+                         :med (get stats 0)
+                         :correct (LineageClassifier/classify stats)}))
+                    SAT-lib))]
+
+    [matches
+     (map (fn [match]
+            (match :SAT-name))
+            matches)]))
+
+(defn text-width
+  "Measure the width, in pixels, of the String text, by the Font and FontMetrics of component."
+  [#^String text #^Component component]
+  ; Get the int[] of widths of the first 256 chars
+  (let [#^ints ws (.getWidths (.getFontMetrics component (.getFont component)))]
+    (reduce (fn [sum c]
+              (if (< (int c) (int 256))
+                (+ sum (aget ws (int c)))
+                sum))
+            0
+            text)))
 
 (defn identify-SAT
   "Takes a calibrated VectorString3D and a list of fiducial points, and checks against the library for identity.
   For consistency in the usage of the Random Forest classifier, the registration is done into the FRT42D-BP106 brain."
   [query-vs fids delta direct substring]
-  (let [matches (match-all query-vs fids delta direct substring)
-        SAT-names (zipmap (range (count matches)) (keys matches))
-        indexed (zipmap (range (count matches)) (vals matches))
+  (let [[matches names] (match-all query-vs fids delta direct substring)
+        SAT-names (vec names)
+        indexed (vec matches)
         column-names ["SAT" "Match" "Seq sim" "Lev Dist" "Med Dist" "Avg Dist" "Cum Dist" "Std Dev" "Prop Mut" "Prop Lengths" "Proximity" "Prox Mut" "Tortuosity"]
         table (JTable. (proxy [AbstractTableModel] []
                 (getColumnName [col]
@@ -150,31 +171,32 @@
                 (getColumnCount []
                   (count column-names))
                 (getValueAt [row col]
-                  (let [match (indexed row)
+                  (let [match (get indexed row)
                         stats (match :stats)]
-                    (str
-                      (cond
-                        (= col 0) (SAT-names row)     ; SAT name
-                        (= col 1) (match :correct)    ; Whether the classifier considered it correct or not
-                        (= col 2) (get stats 6)       ; Similarity
-                        (= col 3) (get stats 5)       ; Levenshtein
-                        (= col 4) (get stats 3)       ; Median Physical Distance
-                        (= col 5) (get stats 0)       ; Average Physical Distance
-                        (= col 6) (get stats 1)       ; Cummulative Physical Distance
-                        (= col 7) (get stats 2)       ; Std Dev
-                        (= col 8) (get stats 4)       ; Prop Mut
-                        (= col 9) (get stats 9)       ; Prop Lengths
-                        (= col 10) (get stats 7)       ; Proximity
-                        (= col 11) (get stats 8)      ; Prox Mut
-                        (= col 12) (get stats 10))))) ; Tortuosity
+                    (cond
+                      (= col 0) (get SAT-names row)
+                      (= col 1) (str (match :correct))    ; Whether the classifier considered it correct or not
+                      true (Utils/cutNumber
+                            (cond
+                              (= col 2) (get stats 6)       ; Similarity
+                              (= col 3) (get stats 5)       ; Levenshtein
+                              (= col 4) (get stats 3)       ; Median Physical Distance
+                              (= col 5) (get stats 0)       ; Average Physical Distance
+                              (= col 6) (get stats 1)       ; Cummulative Physical Distance
+                              (= col 7) (get stats 2)       ; Std Dev
+                              (= col 8) (get stats 4)       ; Prop Mut
+                              (= col 9) (get stats 9)       ; Prop Lengths
+                              (= col 10) (get stats 7)      ; Proximity
+                              (= col 11) (get stats 8)      ; Prox Mut
+                              (= col 12) (get stats 10))    ; Tortuosity
+                            2))))
                 (isCellEditable [row col]
                   false)
                 (setValueAt [ob row col] nil)))
         frame (JFrame. "Matches")
         dummy_ls (LayerSet. (.. Display getFront getProject) (long -1) "Dummy" (double 0) (double 0) (double 0) (double 0) (double 0) (double 512) (double 512) false (int 0) (java.awt.geom.AffineTransform.))]
-
     (.add frame (JScrollPane. table))
-    (.setSize frame (int 500) (int 500))
+    (.setSize frame (int 950) (int 550))
     (.addMouseListener table
                        (proxy [MouseAdapter] []
                          (mousePressed [ev]
@@ -195,9 +217,13 @@
                                    (Display3D/addMesh dummy_ls
                                                       (resample (SAT-lib (match :SAT-name)))
                                                       (match :SAT-name)
-                                                      Color/red))))))))
+                                                      (if (match :correct)
+                                                        Color/red
+                                                        Color/blue)))))))))
+    ; Enlarge the cell width of the first column
+    (.setMinWidth (.. table getColumnModel (getColumn 0)) (int 250))
     (doto frame
-      (.pack)
+      ;(.pack)
       (.setVisible true))))
 
 
