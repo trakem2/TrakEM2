bee2ca2638cf00564421d9d5bc8e5576ce886309
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2009-11-03 17:10:10 +0100

    Fixed CompositeModes to work in the following configurations
    RGBA -> RGBA
    RGB  -> RGBA
    Gray -> RGBA
    
    which is sufficient for images coming from ImageJ which are are either
    type 1 = TYPE_RGB or
    type 10 = TYPE_GRAY
    
    with an optional alpha-channel.

--- ini/trakem2/display/graphics/ColorYCbCrComposite.java
+++ ini/trakem2/display/graphics/ColorYCbCrComposite.java
@@ -1,5 +1,18 @@
 /**
- * 
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package ini.trakem2.display.graphics;
 
@@ -7,13 +20,73 @@ import java.awt.*;
 import java.awt.image.*;
 
 import mpicbg.util.Matrix3x3;
+import mpicbg.util.Util;
 
 /**
- * @author saalfeld
  * 
+ * @author Stephan Saalfeld <saalfeld@mpi-cbg.de>
+ * @version 0.1b
  */
 public class ColorYCbCrComposite implements Composite
 {
+	static private interface Composer
+	{
+		public void compose( final int[] src, final int[] dst, final float alpha );
+	}
+	final static private class ARGB2ARGB implements Composer
+	{
+		final float[] srcYCbCr = new float[ 3 ];
+		final float[] dstYCbCr = new float[ 3 ];
+		
+		final public void compose( final int[] src, final int[] dst, final float alpha )
+		{
+			rgb2ycbcr( src, srcYCbCr );
+			rgb2ycbcr( dst, dstYCbCr );
+			
+			final float srcAlpha = src[ 3 ] / 255.0f * alpha;
+			final float dstAlpha = 1.0f - srcAlpha;
+			
+			dstYCbCr[ 1 ] = srcYCbCr[ 1 ] * srcAlpha + dstYCbCr[ 1 ] * dstAlpha;
+			dstYCbCr[ 2 ] = srcYCbCr[ 2 ] * srcAlpha + dstYCbCr[ 2 ] * dstAlpha;
+			
+			ycbcr2rgb( dstYCbCr, dst );
+			dst[ 3 ] = 255;
+		}
+	}
+	final static private class RGB2ARGB implements Composer
+	{
+		final float[] srcYCbCr = new float[ 3 ];
+		final float[] dstYCbCr = new float[ 3 ];
+		
+		final public void compose( final int[] src, final int[] dst, final float alpha )
+		{
+			rgb2ycbcr( src, srcYCbCr );
+			rgb2ycbcr( dst, dstYCbCr );
+			
+			final float dstAlpha = 1.0f - alpha;
+			
+			dstYCbCr[ 1 ] = srcYCbCr[ 1 ] * alpha + dstYCbCr[ 1 ] * dstAlpha;
+			dstYCbCr[ 2 ] = srcYCbCr[ 2 ] * alpha + dstYCbCr[ 2 ] * dstAlpha;
+			
+			ycbcr2rgb( dstYCbCr, dst );
+			dst[ 3 ] = 255;
+		}
+	}
+	final static private class Gray2ARGB implements Composer
+	{
+		final public void compose( final int[] src, final int[] dst, final float alpha )
+		{
+			final float dstAlpha = 1.0f - alpha;
+			
+			final float l = 0.299f * dst[ 0 ] + 0.587f * dst[ 1 ] + 0.114f * dst[ 2 ];
+			
+			dst[ 0 ] = Math.max( 0, Math.min( 255, Util.round( l * alpha + dst[ 0 ] * dstAlpha ) ) );
+			dst[ 1 ] = Math.max( 0, Math.min( 255, Util.round( l * alpha + dst[ 1 ] * dstAlpha ) ) );
+			dst[ 2 ] = Math.max( 0, Math.min( 255, Util.round( l * alpha + dst[ 2 ] * dstAlpha ) ) );
+			dst[ 3 ] = 255;
+		}
+	}
+	
 	final static float[] rgb2ycbcr = new float[]{
 			0.299f, 0.587f, 0.114f,
 			-0.168736f, -0.331264f, 0.5f,
@@ -76,17 +149,24 @@ public class ColorYCbCrComposite implements Composite
 
 	public CompositeContext createContext( ColorModel srcColorModel, ColorModel dstColorModel, RenderingHints hints )
 	{
-
+		final Composer c;
+		if ( srcColorModel.getNumColorComponents() > 1 )
+		{
+			if ( srcColorModel.hasAlpha() )
+				c = new ARGB2ARGB();
+			else
+				c = new RGB2ARGB();
+		}
+		else
+			c = new Gray2ARGB();
+		
 		return new CompositeContext()
 		{
-
+			private Composer composer = c;
 			public void compose( Raster src, Raster dstIn, WritableRaster dstOut )
 			{
 				final int[] srcPixel = new int[ 4 ];
 				final int[] dstInPixel = new int[ 4 ];
-				
-				final float[] srcYCbCr = new float[ 3 ];
-				final float[] dstInYCbCr = new float[ 3 ];
 				
 				for ( int x = 0; x < dstOut.getWidth(); x++ )
 				{
@@ -95,18 +175,7 @@ public class ColorYCbCrComposite implements Composite
 						src.getPixel( x, y, srcPixel );
 						dstIn.getPixel( x, y, dstInPixel );
 						
-						rgb2ycbcr( srcPixel, srcYCbCr );
-						rgb2ycbcr( dstInPixel, dstInYCbCr );
-						
-						final float srcAlpha = srcPixel[ 3 ] / 255.0f * alpha;
-						final float dstAlpha = 1.0f - srcAlpha;
-						
-						dstInYCbCr[ 0 ] = dstInYCbCr[ 0 ];
-						dstInYCbCr[ 1 ] = srcYCbCr[ 1 ] * srcAlpha + dstInYCbCr[ 1 ] * dstAlpha;
-						dstInYCbCr[ 2 ] = srcYCbCr[ 2 ] * srcAlpha + dstInYCbCr[ 2 ] * dstAlpha;
-						
-						ycbcr2rgb( dstInYCbCr, dstInPixel );
-						dstInPixel[ 3 ] = 255;
+						composer.compose( srcPixel, dstInPixel, alpha );
 						
 						dstOut.setPixel( x, y, dstInPixel );
 					}
@@ -116,7 +185,5 @@ public class ColorYCbCrComposite implements Composite
 			public void dispose()
 			{}
 		};
-
 	}
-
 }
