d00887308090016f40f11ce371f08396f99c00f8
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2009-11-03 18:01:56 +0100

    Fixed CompositeModes to work in the following configurations
    RGBA -> RGBA
    RGB  -> RGBA
    Gray -> RGBA
    
    which is sufficient for images coming from ImageJ which are are either
    type 1 = TYPE_RGB or
    type 10 = TYPE_GRAY
    
    with an optional alpha-channel.
    (cherry picked from commit bee2ca2638cf00564421d9d5bc8e5576ce886309)
    
    Conflicts:
    
    	ini/trakem2/display/graphics/AddARGBComposite.java

--- ini/trakem2/display/graphics/SubtractARGBComposite.java
+++ ini/trakem2/display/graphics/SubtractARGBComposite.java
@@ -1,18 +1,70 @@
 /**
- * 
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package ini.trakem2.display.graphics;
 
 import java.awt.*;
 import java.awt.image.*;
 
+import mpicbg.util.Util;
+
 /**
- * @author saalfeld
  * 
+ * @author Stephan Saalfeld <saalfeld@mpi-cbg.de>
+ * @version 0.1b
  */
 public class SubtractARGBComposite implements Composite
 {
-
+	static private interface Composer
+	{
+		public void compose( final int[] src, final int[] dst, final float alpha );
+	}
+	final static private class ARGB2ARGB implements Composer
+	{
+		final public void compose( final int[] src, final int[] dst, final float alpha )
+		{
+			final float srcAlpha = src[ 3 ] / 255.0f * alpha;
+			
+			dst[ 0 ] = Math.max( 0, Util.round( dst[ 0 ] - src[ 0 ] * srcAlpha ) );
+			dst[ 1 ] = Math.max( 0, Util.round( dst[ 1 ] - src[ 1 ] * srcAlpha ) );
+			dst[ 2 ] = Math.max( 0, Util.round( dst[ 2 ] - src[ 2 ] * srcAlpha ) );
+			dst[ 3 ] = 255;
+		}
+	}
+	final static private class RGB2ARGB implements Composer
+	{
+		final public void compose( final int[] src, final int[] dst, final float alpha )
+		{
+			dst[ 0 ] = Math.max( 0, Util.round( dst[ 0 ] - src[ 0 ] * alpha ) );
+			dst[ 1 ] = Math.max( 0, Util.round( dst[ 1 ] - src[ 1 ] * alpha ) );
+			dst[ 2 ] = Math.max( 0, Util.round( dst[ 2 ] - src[ 2 ] * alpha ) );
+			dst[ 3 ] = 255;
+		}
+	}
+	final static private class Gray2ARGB implements Composer
+	{
+		final public void compose( final int[] src, final int[] dst, final float alpha )
+		{
+			dst[ 0 ] = Math.max( 0, Util.round( dst[ 0 ] - src[ 0 ] * alpha ) );
+			dst[ 1 ] = Math.max( 0, Util.round( dst[ 1 ] - src[ 0 ] * alpha ) );
+			dst[ 2 ] = Math.max( 0, Util.round( dst[ 2 ] - src[ 0 ] * alpha ) );
+			dst[ 3 ] = 255;
+		}
+	}
+	
 	static private SubtractARGBComposite instance = new SubtractARGBComposite();
 
 	final private float alpha;
@@ -35,10 +87,20 @@ public class SubtractARGBComposite implements Composite
 
 	public CompositeContext createContext( ColorModel srcColorModel, ColorModel dstColorModel, RenderingHints hints )
 	{
-
+		final Composer c;
+		if ( srcColorModel.getNumColorComponents() > 1 )
+		{
+			if ( srcColorModel.hasAlpha() )
+				c = new ARGB2ARGB();
+			else
+				c = new RGB2ARGB();
+		}
+		else
+			c = new Gray2ARGB();
+		
 		return new CompositeContext()
 		{
-
+			private Composer composer = c;
 			public void compose( Raster src, Raster dstIn, WritableRaster dstOut )
 			{
 				final int[] srcPixel = new int[ 4 ];
@@ -51,12 +113,7 @@ public class SubtractARGBComposite implements Composite
 						src.getPixel( x, y, srcPixel );
 						dstIn.getPixel( x, y, dstInPixel );
 						
-						final float srcAlpha = srcPixel[ 3 ] / 255.0f * alpha;
-						
-						dstInPixel[ 0 ] = Math.max( 0, Math.round( dstInPixel[ 0 ] - srcPixel[ 0 ] * srcAlpha ) );
-						dstInPixel[ 1 ] = Math.max( 0, Math.round( dstInPixel[ 1 ] - srcPixel[ 1 ] * srcAlpha ) );
-						dstInPixel[ 2 ] = Math.max( 0, Math.round( dstInPixel[ 2 ] - srcPixel[ 2 ] * srcAlpha ) );
-						dstInPixel[ 3 ] = 255;
+						composer.compose( srcPixel, dstInPixel, alpha );
 						
 						dstOut.setPixel( x, y, dstInPixel );
 					}
@@ -66,7 +123,5 @@ public class SubtractARGBComposite implements Composite
 			public void dispose()
 			{}
 		};
-
 	}
-
 }
