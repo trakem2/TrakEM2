84d63562779febdbced28205cf0795db73d9948b
Albert Cardona <albert@mac.(none)>
2009-11-16 10:38:19 -0500

    Renamed to nBlast

--- lineage/Identify.java
+++ lineage/Identify.java
@@ -52,7 +52,7 @@ public class Identify {
 	static private boolean substring = false;
 
 	static public boolean setup() {
-		GenericDialog gd = new GenericDialog("Setup Identify");
+		GenericDialog gd = new GenericDialog("Setup nBLAST");
 		gd.addNumericField("delta:", delta, 2);
 		gd.addCheckbox("direct", direct);
 		gd.addCheckbox("substring", substring);
