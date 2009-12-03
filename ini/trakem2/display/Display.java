a16c6ea2c470e7baab6273fbb2f71cbd25dbf733
Albert Cardona <acardona@ini.phys.ethz.ch>
2009-12-03 12:25:30 -0500

    Proper undo for "Import from text file" Was considering only one layer!

--- ini/trakem2/display/Display.java
+++ ini/trakem2/display/Display.java
@@ -3481,11 +3481,11 @@ public final class Display extends DBObject implements ActionListener, ImageList
 					Display.this.getLayerSet().addChangeTreesStep();
 				}});
 		} else if (command.equals("Import from text file...")) {
-			Display.this.getLayerSet().addLayerContentStep(layer);
+			Display.this.getLayerSet().addChangeTreesStep();
 			Bureaucrat burro = project.getLoader().importImages(layer);
 			if (null != burro)
 				burro.addPostTask(new Runnable() { public void run() {
-					Display.this.getLayerSet().addLayerContentStep(layer);
+					Display.this.getLayerSet().addChangeTreesStep();
 				}});
 		} else if (command.equals("Import labels as arealists...")) {
 			Display.this.getLayerSet().addChangeTreesStep();
