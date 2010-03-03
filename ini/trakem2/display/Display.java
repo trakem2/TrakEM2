982a99c832399dc145dcf8a50ed17f6c707c5dfb
Albert Cardona <acardona@ini.phys.ethz.ch>
2010-03-03 01:49:02 +0100

    Finally updating Display title upon setting calibration.

--- ini/trakem2/display/Display.java
+++ ini/trakem2/display/Display.java
@@ -4092,7 +4092,7 @@ public final class Display extends DBObject implements ActionListener, IJEventLi
 		} else if (command.equals("Calibration...")) {
 			try {
 				IJ.run(canvas.getFakeImagePlus(), "Properties...", "");
-				Display.repaint(getLayerSet());
+				Display.updateTitle(getLayerSet());
 			} catch (RuntimeException re) {
 				Utils.log2("Calibration dialog canceled.");
 			}
