415062950351d4aa468e8c88e9039c903dca0f18
Albert Cardona <acardona@ini.phys.ethz.ch>
2010-04-28 12:29:30 -0400

    New class LazyVirtualStack, generates slices on demand.

---
+++ ini/trakem2/imaging/LazyVirtualStack.java
@@ -0,0 +1,41 @@
+package ini.trakem2.imaging;
+
+import ij.VirtualStack;
+import ij.process.ImageProcessor;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import ini.trakem2.utils.Utils;
+import ini.trakem2.utils.IJError;
+
+
+public class LazyVirtualStack extends VirtualStack {
+	private final List<Callable<ImageProcessor>> tasks = new ArrayList<Callable<ImageProcessor>>();
+	private int initial_size;
+	public LazyVirtualStack(final int width, final int height, final int initial_size) {
+		super();
+		Utils.setField(this, ij.ImageStack.class, "width", width);
+		Utils.setField(this, ij.ImageStack.class, "height", width);
+	}
+
+	public void addSlice(String name) {
+		throw new UnsupportedOperationException("LazyVirtualStack accepts Callable<ImageProcessor> slices only.");
+	}
+	public void deleteSlice(int i) {
+		throw new UnsupportedOperationException("LazyVirtualStack: can't remove slices.");
+	}
+	public void addSlice(final Callable<ImageProcessor> task) {
+		tasks.add(task);
+	}
+	public ImageProcessor getProcessor(final int n) {
+		try {
+			return tasks.get(n-1).call();
+		} catch (Exception e) {
+			IJError.print(e);
+		}
+		return null;
+	}
+	public int getSize() {
+		return Math.max(initial_size, tasks.size());
+	}
+}
