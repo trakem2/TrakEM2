2d6ba66abb7dfa108d8a07c4f4044246ac41b16f
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-03-21 19:59:25 +0100

    Fully compliant NeuroML and MorphML 1.8.3.
    NeuroML exports with synapses,
    and MorphML only the morphology of the arbors.

--- ini/trakem2/io/NeuroML.java
+++ ini/trakem2/io/NeuroML.java
@@ -2,6 +2,8 @@ package ini.trakem2.io;
 
 import ij.measure.Calibration;
 import ini.trakem2.display.AreaTree;
+import ini.trakem2.display.Connector;
+import ini.trakem2.display.Displayable;
 import ini.trakem2.display.Node;
 import ini.trakem2.display.Tree;
 import ini.trakem2.display.Treeline;
@@ -16,6 +18,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 import java.util.Set;
 
 public final class NeuroML {
@@ -48,11 +51,11 @@ public final class NeuroML {
 			w.write("\n");
 			w.write(t.getAnnotation());
 		}
-		w.write(" </meta:notes>\n");
+		w.write("</meta:notes>\n");
 		w.write(" <meta:properties>\n");
 		w.write("  <meta:property tag=\"Neuron type\" value=\"manually reconstructed\" />\n");
 		w.write(" </meta:properties>\n");
-		w.write(" <segments>\n");
+		w.write(" <segments xmlns=\"http://morphml.org/morphml/schema\">\n");
 	}
 
 	/** Transform in 2d the point with the given affine that combines the Tree affine and the calibration,
@@ -120,17 +123,6 @@ public final class NeuroML {
 		w.write("\"/>\n  </segment>\n");
 	}
 
-	static private final class Synapse {
-		final long srcCellId, srcSegmentId, tgtCellId, tgtSegmentId;
-		Synapse(final long srcCellId, final long srcSegmentId,
-				final long tgtCellId, final long tgtSegmentId) {
-			this.srcCellId = srcCellId;
-			this.srcSegmentId = srcSegmentId;
-			this.tgtCellId = tgtCellId;
-			this.tgtSegmentId = tgtSegmentId;
-		}
-	}
-
 	static private final String units(final Tree<?> d) {
 		final Calibration cal = d.getLayerSet().getCalibration();
 		final String unit = cal.getUnit().toLowerCase();
@@ -138,29 +130,59 @@ public final class NeuroML {
 		if (unit.equals("µm") || unit.matches("^micrometers*$")) return "micrometers";
 		return cal.getUnits();
 	}
+
 	
-	static public final void exportMorphML(final Collection<Tree<?>> trees, final Writer w) throws Exception {
-		exportMorphML(new HashSet<Tree<?>>(trees), w);
+	static private final class HalfSynapse {
+		private Connector c;
+		private Tree<?> t;
+		private Node<?> node;
+		private long segmentId;
+
+		/** A pre- or a post-synaptic site, located at {@param node} with which a segment with id {@param segmentId} was made.*/
+		HalfSynapse(final Connector c, final Tree<?> t, final Node<?> node, final long segmentId) {
+			this.c = c;
+			this.t = t;
+			this.node = node;
+			this.segmentId = segmentId;
+		}
+	}
+	
+	static private final class Synapse {
+		private final HalfSynapse pre, post;
+		Synapse(final HalfSynapse pre, final HalfSynapse post) {
+			this.pre = pre;
+			this.post = post;
+		}
 	}
 
-	static public final void exportMorphML(final Set<Tree<?>> trees, final Writer w) throws Exception
+	static private final void collectConnectors(final Node<?> node, final Tree<?> t,
+			final float[] nodeWorldCoords, final long segmentId,
+			final List<HalfSynapse> pre, final List<HalfSynapse> post)
 	{
-		if (trees.isEmpty()) return;
-		
-		// Header
-		w.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-		w.write("<morphml xmlns=\"http://morphml.org/morphml/schema\"\n");
-		w.write("  xmlns:meta=\"http://morphml.org/metadata/schema\"\n");
-		w.write("  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n");
-		w.write("  xsi:schemaLocation=\"http://morphml.org/morphml/schema http://www.neuroml.org/NeuroMLValidator/NeuroMLFiles/Schemata/v1.8.1/Level1/MorphML_v1.8.1.xsd\"\n");
-		w.write("  length_units=\"micrometers\">\n<cells>\n");
-		
-		final Set<Synapse> synapses = new HashSet<Synapse>();
-		
+		for (final Displayable d : t.getLayerSet().findZDisplayables(Connector.class, node.getLayer(), (int)nodeWorldCoords[0], (int)nodeWorldCoords[1], false)) {
+			final Connector c = (Connector) d;
+			if (c.intersectsOrigin(nodeWorldCoords[0], nodeWorldCoords[1], node.getLayer())) {
+				pre.add(new HalfSynapse(c, t, node, segmentId));
+			} else {
+				post.add(new HalfSynapse(c, t, node, segmentId));
+			}
+		}
+	}
+	
+	static private final class TreePair {
+		final Tree<?> source, target;
+		TreePair(Tree<?> source, Tree<?> target) {
+			this.source = source;
+			this.target = target;
+		}
+		public final boolean equals(final Object ob) {
+			final TreePair p = (TreePair) ob;
+			return (source == p.source && target == p.target);
+		}
+	}
 
-		// Scale units to micrometers
+	static private final double scaleToMicrometers(final Calibration cal) {
 		final double scale;
-		final Calibration cal = trees.iterator().next().getLayerSet().getCalibration();
 		final String unit = cal.getUnit().trim().toLowerCase();
 		if (unit.equals("nanometer") || unit.equals("nm")) scale = 0.001;
 		else if (unit.equals("micrometer") || unit.equals("µm") || unit.equals("um")) scale = 1;
@@ -170,6 +192,27 @@ public final class NeuroML {
 			scale = 1;
 			Utils.logAll("UNKNOWN unit '" + unit + "' -- using scale of 1.");
 		}
+		return scale;
+	}
+	
+	static public final void exportMorphML(final Collection<Tree<?>> trees, final Writer w) throws Exception {
+		exportMorphML(new HashSet<Tree<?>>(trees), w);
+	}
+
+	static public final void exportMorphML(final Set<Tree<?>> trees, final Writer w) throws Exception {
+		if (trees.isEmpty()) return;
+		
+		// Header
+		w.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+		w.write("<morphml xmlns=\"http://morphml.org/morphml/schema\"\n");
+		w.write("  xmlns:meta=\"http://morphml.org/metadata/schema\"\n");
+		w.write("  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n");
+		w.write("  xsi:schemaLocation=\"http://morphml.org/morphml/schema http://www.neuroml.org/NeuroMLValidator/NeuroMLFiles/Schemata/v1.8.1/Level1/MorphML_v1.8.1.xsd\"\n");
+		w.write("  length_units=\"micrometer\">\n<cells>\n");
+		
+		// Scale units to micrometers
+		final Calibration cal = trees.iterator().next().getLayerSet().getCalibration();
+		final double scale = scaleToMicrometers(cal);
 		
 		final AffineTransform scale2d = new AffineTransform(cal.pixelWidth * scale, 0, 0, cal.pixelHeight * scale, 0, 0);
 		final double zScale = cal.pixelWidth * scale; // not pixelDepth
@@ -179,36 +222,145 @@ public final class NeuroML {
 		{
 			if (null == t.getRoot()) continue;
 	
-			exportMorphMLCell(t, w, trees, synapses, scale2d, zScale);
+			exportMorphMLCell(w, t, trees, null, null, scale2d, zScale);
 		}
 		
-		// TODO write a project group with all the synapses among the members of the trees set.
-
 		w.write("</cells>\n</morphml>\n");
+	}
+	
+	static public final void exportNeuroML(final Collection<Tree<?>> trees, final Writer w) throws Exception {
+		exportNeuroML(new HashSet<Tree<?>>(trees), w);
+	}
+
+	/** Export to NeuroML 1.8.3, with synapses.
+	 * Every {@link Tree} is represented by a &lt;cell&gt;, and an instance of that &lt;cell&gt;
+	 * is represented by a &lt;population&gt; of one single cell. */
+	static public final void exportNeuroML(final Set<Tree<?>> trees, final Writer w) throws Exception
+	{
+		if (trees.isEmpty()) return;
+		
+		// Header
+		w.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+		 + "<neuroml xmlns=\"http://morphml.org/neuroml/schema\"\n"
+		 + " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
+		 + " xmlns:net=\"http://morphml.org/networkml/schema\"\n" 
+		 + " xmlns:mml=\"http://morphml.org/morphml/schema\"\n"
+		 + " xmlns:meta=\"http://morphml.org/metadata/schema\"\n"
+		 + " xmlns:bio=\"http://morphml.org/biophysics/schema\"\n"
+		 + " xmlns:cml=\"http://morphml.org/channelml/schema\"\n"
+		 + " xsi:schemaLocation=\"http://morphml.org/neuroml/schema http://www.neuroml.org/NeuroMLValidator/NeuroMLFiles/Schemata/v1.8.1/Level3/NeuroML_Level3_v1.8.1.xsd\"\n"
+		 + " length_units=\"micrometer\">\n");
+		
+		final List<HalfSynapse> presynaptic = new ArrayList<HalfSynapse>();
+		final List<HalfSynapse> postsynaptic = new ArrayList<HalfSynapse>();
+
+		// Scale units to micrometers
+		final Calibration cal = trees.iterator().next().getLayerSet().getCalibration();
+		final double scale = scaleToMicrometers(cal);
+		
+		final AffineTransform scale2d = new AffineTransform(cal.pixelWidth * scale, 0, 0, cal.pixelHeight * scale, 0, 0);
+		final double zScale = cal.pixelWidth * scale; // not pixelDepth
+		
+		w.write("<cells>\n");
+		
+		// Each Tree is a cell
+		for (final Tree<?> t : trees)
+		{
+			if (null == t.getRoot()) continue;
+	
+			exportMorphMLCell(w, t, trees, presynaptic, postsynaptic, scale2d, zScale);
+		}
+		
+		w.write("</cells>\n");
+
+		// Write a a population of cell for every Tree, where each population has only one cell at 0,0,0.
+		// If the id=10, then the name is p10 and the type is t10.
+		w.write("<populations xmlns=\"http://morphml.org/networkml/schema\">\n");
+		for (final Tree<?> t : trees) {
+			w.write(" <population name=\"p");
+			final String sid = Long.toString(t.getId());
+			w.write(sid);
+			w.write("\" cell_type=\"t");
+			w.write(sid);
+			w.write("\">\n  <instances size=\"1\">\n   <instance id=\"0\"><location x=\"0\" y=\"0\" z=\"0\"/></instance>\n  </instances>\n </population>\n");
+		}
+		w.write("</populations>\n");
+
+		// Write a project group with all the synapses among the members of the set of trees.
+		w.write("<projections units=\"Physiological Units\" xmlns=\"http://morphml.org/networkml/schema\">\n");
+
+		// Figure out which pre connect to which post: the Connector instance is shared, so use it as key
+		final Map<Connector,HalfSynapse> cpre = new HashMap<Connector,HalfSynapse>();
+		for (final HalfSynapse syn : presynaptic) {
+			cpre.put(syn.c, syn);
+		}
+		final Map<TreePair,List<Synapse>> pairs = new HashMap<TreePair,List<Synapse>>();
+		for (final HalfSynapse post : postsynaptic) {
+			final HalfSynapse pre = cpre.get(post.c);
+			if (null == pre) continue; // Does not originate within the set of trees
+			// pre and post share the same Connector
+			final TreePair pair = new TreePair(pre.t, post.t);
+			List<Synapse> ls = pairs.get(pair);
+			if (null == ls) {
+				ls = new ArrayList<Synapse>();
+				pairs.put(pair, ls);
+			}
+			ls.add(new Synapse(pre, post));
+		}
+
+		for (final Map.Entry<TreePair,List<Synapse>> e : pairs.entrySet()) {
+			// Write synapse between pre and post
+			final TreePair pair = e.getKey();
+			w.write("  <projection name=\"NetworkConnection\" source=\"p");
+			w.write(Long.toString(pair.source.getId()));
+			w.write("\" target=\"p");
+			w.write(Long.toString(pair.target.getId()));
+			w.write("\">\n");
+			w.write("   <synapse_props synapse_type=\"DoubExpSynA\" internal_delay=\"5\" weight=\"1\" threshold=\"-20\"/>\n");
+			w.write("   <connections size=\"");
+			final List<Synapse> ls = e.getValue();
+			w.write(Integer.toString(ls.size()));
+			w.write("\">\n");
+			int cid = 0;
+			for (final Synapse syn : ls) {
+				w.write("    <connection id=\"");
+				w.write(Integer.toString(cid));
+				w.write("\" pre_cell_id=\"0\" pre_segment_id=\"");
+				w.write(Long.toString(syn.pre.segmentId));
+				w.write("\" pre_fraction_along=\"0.5\" post_cell_id=\"0\" post_segment_id=\"");
+				w.write(Long.toString(syn.post.segmentId));
+				w.write("\"/>\n");
+				
+				cid += 1;
+			}
+			w.write("   </connections>\n");
+			
+			w.write("  </projection>\n");
+		}
+		w.write(" </projections>\n");
+
+		w.write("</neuroml>\n");
 	}
 
 	/** Without headers, just the cell block for a single AreaTree.
 	 *  Works by duplicating the AreaTree as a Treeline. */
-	static private final void exportMorphMLCell(final Tree<?> t, final Writer w,
-			Set<Tree<?>> trees, Set<Synapse> synapses,
+	static private final void exportMorphMLCell(final Writer w, final Tree<?> t,
+			final Set<Tree<?>> trees, final List<HalfSynapse> pre, final List<HalfSynapse> post,
 			final AffineTransform scale2d, final double zScale) throws Exception
 	{
 		if (t instanceof Treeline) {
-			exportMorphMLCell((Treeline)t, w, trees, synapses, scale2d, zScale);
+			exportMorphMLCell(w, (Treeline)t, trees, pre, post, scale2d, zScale);
 		} else if (t instanceof AreaTree) {
-			exportMorphMLCell(Tree.copyAs((AreaTree)t, Treeline.class, Treeline.RadiusNode.class), w, trees, synapses, scale2d, zScale);
+			exportMorphMLCell(w, Tree.copyAs((AreaTree)t, Treeline.class, Treeline.RadiusNode.class), trees, pre, post, scale2d, zScale);
 		}
 	}
 
-	/** Without headers, just the cell block for a single Treeline. */
-	static private final void exportMorphMLCell(final Treeline t, final Writer w,
-			Set<Tree<?>> trees, Set<Synapse> synapses,
+	/** Without headers, just the cell block for a single Treeline.
+	 * If pre is null, then synapses are not collected. */
+	static private final void exportMorphMLCell(final Writer w, final Treeline t,
+			final Set<Tree<?>> trees, final List<HalfSynapse> pre, final List<HalfSynapse> post,
 			final AffineTransform scale2d, final double zScale) throws IOException
-	{
-		
-		// TODO collect synapses into the set
-		
-		
+	{	
 		final float[] fp = new float[4]; // x, y, z, r
 
 		// Prepare transform
@@ -226,24 +378,24 @@ public final class NeuroML {
 		final HashMap<Node<Float>,float[]> nodeCoords = new HashMap<Node<Float>,float[]>();
 
 		// Root gets ID of 0:
-		long nextId = 0;
+		long nextSegmentId = 0;
+		long cableId = 0;
 		final Node<Float> root = t.getRoot();
 
 		toPoint(root, fp, aff, zScale);
-		writeSomaSegment(w, fp); // a dummy segment that has no length
-
-		// Cable ids
-		long cableId = 0;
+		writeSomaSegment(w, fp); // a dummy segment that has no length, and with a cableId of 0.
+		if (null != pre) collectConnectors(root, t, fp, 0, pre, post);
 
 		// Prepare
-		cableId += 1;
-		nodeIds.put(root, nextId++);
+		nodeIds.put(root, nextSegmentId);
 		nodeCoords.put(root, fp.clone());
+		nextSegmentId += 1;
+		cableId += 1;
 
 		// All cables that come out of the Soma (the root) require a special tag:
 		final HashSet<Long> somaCables = new HashSet<Long>();
 
-		// Iterate all cables (all slabs)
+		// Iterate all cables (all slabs; here a slab is synonym with cable, even if in NeuroML it doesn't have to be)
 		for (final Node<Float> node : t.getRoot().getBranchAndEndNodes()) {
 			// Gather the list of nodes all the way up to the previous branch node or root,
 			// that last one not included.
@@ -264,10 +416,12 @@ public final class NeuroML {
 				final Node<Float> seg = it.previous();
 				// Write the segment
 				toPoint(seg, fp, aff, zScale);
-				writeCableSegment(w, fp, nextId, parentId, parentCoords, sCableId);
+				writeCableSegment(w, fp, nextSegmentId, parentId, parentCoords, sCableId);
+				// Inspect and collect synapses originating at this node
+				if (null != pre) collectConnectors(seg, t, fp, nextSegmentId, pre, post);
 				// Prepare next segment in the cable
-				parentId = nextId;
-				nextId += 1;
+				parentId = nextSegmentId;
+				nextSegmentId += 1;
 				parentCoords = null; // is used only for the first node
 			}
 			// Record the branch node, to be used for filling in "distal" fields
@@ -286,7 +440,7 @@ public final class NeuroML {
 
 		// Define the nature of each cable
 		// Each cable requires a unique name
-		w.write(" <cables>\n");
+		w.write(" <cables xmlns=\"http://morphml.org/morphml/schema\">\n");
 		w.write("  <cable id=\"0\" name=\"Soma\">\n   <meta:group>soma_group</meta:group>\n  </cable>\n");
 		for (long i=1; i<cableId; i++) {
 			final String sid = Long.toString(i);
