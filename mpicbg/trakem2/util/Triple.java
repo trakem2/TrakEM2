642028730ed999cc8b44d6f98c41c0089611842c
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-10-13 20:16:45 +0200

    New Triple Tuple.

---
+++ mpicbg/trakem2/util/Triple.java
@@ -0,0 +1,15 @@
+package mpicbg.trakem2.util;
+
+public class Triple<A, B, C>
+{
+	public final A a;
+	public final B b;
+	public final C c;
+
+	public Triple(A a, B b, C c)
+	{
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+}
