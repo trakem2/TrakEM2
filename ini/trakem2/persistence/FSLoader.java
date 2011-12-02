3ca156518cbd05fa0e0ba5511f0a8be3e8cea2b9
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-12-02 04:49:04 -0500

    removed obsolete message.

--- ini/trakem2/persistence/FSLoader.java
+++ ini/trakem2/persistence/FSLoader.java
@@ -1720,10 +1720,6 @@ public final class FSLoader extends Loader {
 		if (null != srmode) resizing_mode = Loader.getMode(srmode);
 
 		try {
-			// Now:
-			// 1 - Ask the Patch to apply a coordinate transform, or rather, create a function that gets the coordinate transform from the Patch and applies it to the 'ip'.
-			// 2 - Then (1) should return both the transformed image and the alpha mask
-
 			ImageProcessor ip;
 			ByteProcessor alpha_mask = null;
 			ByteProcessor outside_mask = null;
