eba54df61b7bb471a33c21b40bb3300378b8c703
Albert Cardona <acardona@ini.phys.ethz.ch>
2012-01-30 06:42:43 -0500

    Patch.exportFlatImage respects masks if desired.

--- ini/trakem2/display/Patch.java
+++ ini/trakem2/display/Patch.java
@@ -85,6 +85,7 @@ import mpicbg.trakem2.transform.CoordinateTransform;
 import mpicbg.trakem2.transform.CoordinateTransformList;
 import mpicbg.trakem2.transform.TransformMesh;
 import mpicbg.trakem2.transform.TransformMeshMapping;
+import mpicbg.trakem2.transform.TransformMeshMappingWithMasks;
 import mpicbg.trakem2.transform.TransformMeshMappingWithMasks.ImageProcessorWithMasks;
 
 public final class Patch extends Displayable implements ImageData {
@@ -1718,8 +1719,29 @@ public final class Patch extends Displayable implements ImageData {
 	 *  @param patches The list of patches to paint. The first gets painted first (at the bottom).
 	 *  @param background The color with which to paint the outsides where no image paints into.
 	 *  @param setMinAndMax defines whether the min and max of each Patch is set before pasting the Patch.
+	 *
+	 * For exporting while blending the display ranges (min,max) {@see ExportUnsignedShort}.
 	 */
 	static public ImageProcessor makeFlatImage(final int type, final Layer layer, final Rectangle srcRect, final double scale, final Collection<Patch> patches, final Color background, final boolean setMinAndMax) {
+		return makeFlatImage(type, layer, srcRect, scale, patches, background, setMinAndMax, false)[0];
+	}
+
+	/**
+	 * 
+	 * @param type
+	 * @param layer
+	 * @param srcRect
+	 * @param scale
+	 * @param patches
+	 * @param background
+	 * @param setMinAndMax
+	 * @param with_masks
+	 * @return An array of ImageProcessor where [0] is the image and [1] the mask.
+	 * 
+	 * For exporting while blending the display ranges (min,max) {@see ExportUnsignedShort}.
+	 */
+	static public ImageProcessor[] makeFlatImage(final int type, final Layer layer, final Rectangle srcRect, final double scale, final Collection<Patch> patches, final Color background, final boolean setMinAndMax, final boolean with_masks) {
+		
 		final ImageProcessor ip;
 		final int W, H;
 		if (scale < 1) {
@@ -1752,7 +1774,14 @@ public final class Patch extends Displayable implements ImageData {
 			ip.setColor(background);
 			ip.fill();
 		}
-
+		
+		////
+		ImageProcessorWithMasks ipwm = null;
+		if (with_masks) {
+			ipwm = new ImageProcessorWithMasks(ip, null, new ByteProcessor(ip.getWidth(), ip.getHeight()));
+		}
+		////
+		
 		AffineModel2D sc = null;
 		if ( scale < 1.0 )
 		{
@@ -1788,7 +1817,20 @@ public final class Patch extends Displayable implements ImageData {
 			if (null != sc) patch_affine.preConcatenate( sc );
 
 			final CoordinateTransformMesh mesh = new CoordinateTransformMesh( list, p.meshResolution, p.getOWidth(), p.getOHeight() );
-			final mpicbg.ij.TransformMeshMapping<CoordinateTransformMesh> mapping = new mpicbg.ij.TransformMeshMapping<CoordinateTransformMesh>( mesh );
+			
+			mpicbg.ij.TransformMeshMapping<CoordinateTransformMesh> mapping = null;
+			TransformMeshMappingWithMasks<CoordinateTransformMesh> mappingWithMasks = null;
+			
+			ByteProcessor mask = null;
+			
+			if (with_masks) {
+				mask = p.project.getLoader().fetchImageMask(p);
+			}
+			if (null == mask) {
+				mapping = new mpicbg.ij.TransformMeshMapping<CoordinateTransformMesh>( mesh );
+			} else {
+				mappingWithMasks = new TransformMeshMappingWithMasks<CoordinateTransformMesh>( mesh );
+			}
 			
 			// 4. Convert the patch to the required type
 			ImageProcessor pi = p.getImageProcessor();
@@ -1814,11 +1856,16 @@ public final class Patch extends Displayable implements ImageData {
 			
 			/* TODO for taking into account independent min/max setting for each patch,
 			 * we will need a mapping with an `intensity transfer function' to be implemented.
+			 * --> EXISTS already as mpicbg/trakem2/transform/ExportUnsignedShort.java
 			 */
-			mapping.mapInterpolated( pi, ip );
+			if (null != mapping) {
+				mapping.mapInterpolated( pi, ip );
+			} else {
+				mappingWithMasks.mapInterpolated(new ImageProcessorWithMasks(pi, null, mask), ipwm);
+			}
 		}
 
-		return ip;
+		return new ImageProcessor[]{ipwm.ip, ipwm.outside};
 	}
 
 	/** Make the border have an alpha of zero. */
