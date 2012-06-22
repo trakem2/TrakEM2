28601f5830d547c8443d6d48fe40271c3ed349ec
Albert Cardona <sapristi@gmail.com>
2012-06-22 13:34:55 -0400

    New IFilter EnhanceContrast, runs ImageProcessor.stretchHistogram with all booleans set to false, so only the min,max gets set without altering the data.

---
+++ ini/trakem2/imaging/filters/EnhanceContrast.java
@@ -0,0 +1,43 @@
+package ini.trakem2.imaging.filters;
+
+import java.util.Map;
+
+import ij.measure.Measurements;
+import ij.plugin.ContrastEnhancer;
+import ij.process.ImageProcessor;
+import ij.process.ImageStatistics;
+
+/** Uses the @{link {@link ContrastEnhancer#stretchHistogram(ImageProcessor, double, ImageStatistics)} function. */
+public class EnhanceContrast implements IFilter
+{
+	/** Percent of saturated pixels to leave outside the min, max range. */
+	protected double s = 0.4;
+	
+	public EnhanceContrast() {}
+	
+	public EnhanceContrast(Map<String,String> params) {}
+
+	@Override
+	public ImageProcessor process(ImageProcessor ip) {
+		// Will not alter ip, no need to duplicate
+		new ContrastEnhancer().stretchHistogram(ip, s, ImageStatistics.getStatistics(ip, Measurements.MIN_MAX, null));
+		return ip;
+	}
+
+	@Override
+	public String toXML(String indent) {
+		return new StringBuilder(indent)
+			.append("<t2_filter class=\"").append(getClass().getName())
+			.append("\" s=\"").append(s)
+			.append("\" />\n").toString();
+	}
+	
+	@Override
+	public boolean equals(final Object o) {
+		if (null == o) return false;
+		if (o.getClass() == EnhanceContrast.class) {
+			return ((EnhanceContrast)o).s == this.s;
+		}
+		return false;
+	}
+}
