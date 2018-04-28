106275fbe7f34e0c8119264ba9f3db8b0e356424
Curtis Rueden <ctrueden@wisc.edu>
2018-04-27 17:45:33 -0700

    Update Travis CI configuration

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh
+sh travis-build.sh $encrypted_a6f5892f197d_key $encrypted_a6f5892f197d_iv
---
+++ README.md
@@ -0,0 +1,2 @@
+[![](https://travis-ci.org/trakem2/TrakEM2.svg?branch=master)](https://travis-ci.org/trakem2/TrakEM2)
+
