1872b0a637ad0b34613cb74188602b7f50d5c6fd
Curtis Rueden <ctrueden@wisc.edu>
2021-09-15 13:37:49 -0500

    Remove obsolete copyright notices

--- src/main/java/ini/trakem2/utils/IJError.java
+++ src/main/java/ini/trakem2/utils/IJError.java
@@ -19,27 +19,7 @@
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-/**
 
-TrakEM2 plugin for ImageJ(C).
-Copyright (C) 2005-2009 Albert Cardona and Rodney Douglas.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation (http://www.gnu.org/licenses/gpl.txt )
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
-
-You may contact Albert Cardona at acardona at ini.phys.ethz.ch
-Institute of Neuroinformatics, University of Zurich / ETH, Switzerland.
-**/
 
 package ini.trakem2.utils;
 
